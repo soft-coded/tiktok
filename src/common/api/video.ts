de426cd5c624b9ac36ef7aae63f76e2672ccd343
soft-coded <shrutanten.work@gmail.com>
2021-12-04 20:55:53 +0530

    Finish video upload

--- src/common/api/video.ts
+++ src/common/api/video.ts
@@ -5,7 +5,6 @@ const videoURL = "/video";
 export const createVideo = (data: FormData) =>
 	apiClient.post(videoURL + "/create", data, {
 		headers: {
-			"Content-Type": "multipart/form-data",
-			Accept: "video/mp4"
+			"Content-Type": "multipart/form-data"
 		}
 	});
