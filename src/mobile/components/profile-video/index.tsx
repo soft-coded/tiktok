31ef47a1936ff1749c866348e5dfabbafa01c786
soft-coded <shrutanten.work@gmail.com>
2022-01-13 22:16:55 +0530

    Finish profile page videos

---
+++ src/mobile/components/profile-video/index.tsx
@@ -0,0 +1,84 @@
+import { useEffect, useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+import classes from "./profile-video.module.scss";
+import LoadingSpinner from "../../../common/components/loading-spinner";
+import constants from "../../../common/constants";
+
+interface Props {
+	video: string;
+}
+
+export default function ProfileVideo({ video }: Props) {
+	const navigate = useNavigate();
+	const [showSpinner, setShowSpinner] = useState(false);
+	const containerRef = useRef<HTMLDivElement>(null);
+	const videoRef = useRef<HTMLVideoElement>(null);
+
+	useEffect(() => {
+		if (!containerRef.current || !videoRef.current) return;
+		const container = containerRef.current;
+		const vid = videoRef.current;
+		let playTimeout: NodeJS.Timeout,
+			isPlaying = false,
+			cancelClick = false;
+
+		function handleClick() {
+			if (cancelClick) return;
+			navigate("/video/" + video);
+		}
+		function handleTouchStart() {
+			playTimeout = setTimeout(() => {
+				vid.play();
+				isPlaying = true;
+				cancelClick = true;
+			}, 300);
+		}
+		function handleTouchEnd() {
+			clearTimeout(playTimeout);
+			if (isPlaying) {
+				vid.pause();
+				isPlaying = false;
+			} else cancelClick = false;
+		}
+		function toggleSpinnerOn() {
+			setShowSpinner(true);
+		}
+		function toggleSpinnerOff() {
+			setShowSpinner(false);
+		}
+		function disableContextMenu(e: Event) {
+			e.preventDefault();
+		}
+
+		container.addEventListener("click", handleClick);
+		container.addEventListener("touchstart", handleTouchStart);
+		container.addEventListener("touchend", handleTouchEnd);
+		vid.addEventListener("waiting", toggleSpinnerOn);
+		vid.addEventListener("playing", toggleSpinnerOff);
+		vid.addEventListener("contextmenu", disableContextMenu);
+
+		return () => {
+			container.removeEventListener("click", handleClick);
+			container.removeEventListener("touchstart", handleTouchStart);
+			container.removeEventListener("touchend", handleTouchEnd);
+			vid.removeEventListener("waiting", toggleSpinnerOn);
+			vid.removeEventListener("playing", toggleSpinnerOff);
+			vid.removeEventListener("contextmenu", disableContextMenu);
+		};
+	}, [navigate, video]);
+
+	return (
+		<div ref={containerRef} className={classes["video"]}>
+			<video
+				ref={videoRef}
+				src={constants.videoLink + "/" + video}
+				preload="metadata"
+				loop
+				playsInline
+				muted
+			/>
+			{showSpinner && <LoadingSpinner className={classes["spinner"]} />}
+		</div>
+	);
+}
--- src/mobile/pages/profile/index.tsx
+++ src/mobile/pages/profile/index.tsx
@@ -9,6 +9,7 @@ import { errorNotification } from "../../helpers/error-notification";
 import { getUser } from "../../../common/api/user";
 import LoadingSpinner from "../../../common/components/loading-spinner";
 import { joinClasses } from "../../../common/utils";
+import ProfileVideo from "../../components/profile-video";
 
 interface Props {
 	isOwn?: boolean;
@@ -19,6 +20,9 @@ export default function Profile({ isOwn }: Props) {
 	const dispatch = useAppDispatch();
 	const [user, setUser] = useState<UserData | null>();
 	const loggedInAs = useAppSelector(state => state.auth.username);
+	const [videosType, setVideosType] = useState<"uploaded" | "liked">(
+		"uploaded"
+	);
 
 	useEffect(() => {
 		errorNotification(
@@ -38,45 +42,62 @@ export default function Profile({ isOwn }: Props) {
 			{!user ? (
 				<LoadingSpinner />
 			) : (
-				<div className="profile-header">
-					<header>
-						<div />
-						<h4>{user.name}</h4>
-						<div>
-							<i
-								className={joinClasses(
-									"fas",
-									isOwn ? "fa-ellipsis-h" : "fa-share"
-								)}
-							/>
+				<>
+					<div className="profile-header">
+						<header>
+							<div />
+							<h4>{user.name}</h4>
+							<div>
+								<i
+									className={joinClasses(
+										"fas",
+										isOwn ? "fa-ellipsis-h" : "fa-share"
+									)}
+								/>
+							</div>
+						</header>
+						<div className="user-info">
+							<div className="rounded-photo">
+								<img
+									src={constants.pfpLink + "/" + user.username}
+									alt={user.name}
+								/>
+							</div>
+							<h4>@{user.username}</h4>
+							<ul>
+								<li className="show-divider">
+									<strong>{user.following}</strong>
+									<span>Following</span>
+								</li>
+								<li className="show-divider">
+									<strong>{user.followers}</strong>
+									<span>{user.followers === 1 ? "Follower" : "Followers"}</span>
+								</li>
+								<li>
+									<strong>{user.totalLikes}</strong>
+									<span>{user.totalLikes === 1 ? "Like" : "Likes"}</span>
+								</li>
+							</ul>
+							{!isOwn && <button className="primary-button">Follow</button>}
+							<p className="break-word description">{user.description}</p>
 						</div>
-					</header>
-					<div className="user-info">
-						<div className="rounded-photo">
-							<img
-								src={constants.pfpLink + "/" + user.username}
-								alt={user.name}
-							/>
-						</div>
-						<h4>@{user.username}</h4>
-						<ul>
-							<li className="show-divider">
-								<strong>{user.following}</strong>
-								<span>Following</span>
-							</li>
-							<li className="show-divider">
-								<strong>{user.followers}</strong>
-								<span>{user.followers === 1 ? "Follower" : "Followers"}</span>
-							</li>
-							<li>
-								<strong>{user.totalLikes}</strong>
-								<span>{user.totalLikes === 1 ? "Like" : "Likes"}</span>
-							</li>
-						</ul>
-						{!isOwn && <button className="primary-button">Follow</button>}
-						<p className="break-word description">{user.description}</p>
 					</div>
-				</div>
+					<div className="video-buttons">
+						<button
+							className={videosType === "uploaded" ? "active" : undefined}
+						>
+							<i className="fas fa-video" />
+						</button>
+						<button className={videosType === "liked" ? "active" : undefined}>
+							<i className="fas fa-heart" />
+						</button>
+					</div>
+					<div className="videos-container">
+						{user.videos!.map((vid, i) => (
+							<ProfileVideo key={i} video={vid as string} />
+						))}
+					</div>
+				</>
 			)}
 		</div>
 	);
